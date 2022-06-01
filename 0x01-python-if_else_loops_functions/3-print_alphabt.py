bf99eadd394f8bd4f964ec5146291ef8c961960f
Veralee225 <Veralee225@gmail.com>
2022-06-01 02:23:35 +0100

    print alphabet

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
-import string
-for alpha_letters in string.ascii_lowercase:
-    if alpha_letters not in 'qe':
-       print(alpha_letters, end="")  # no need for format: the format is already OK
+for i in 'abcdefghijklmnopqrstuvwxyz':
+    if (i != "q" and i != "e"):
+        print("{}".format(i), end="")

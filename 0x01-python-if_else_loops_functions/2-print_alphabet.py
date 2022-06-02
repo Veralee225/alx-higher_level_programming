2837791ee55e0ad5304fe2bd43078614260ee3b9
Veralee225 <Veralee225@gmail.com>
2022-06-02 01:15:52 +0100

    I made changes

--- 0x01-python-if_else_loops_functions/2-print_alphabet.py
+++ 0x01-python-if_else_loops_functions/2-print_alphabet.py
@@ -1,5 +1,3 @@
 #!/usr/bin/python3
-for alpha_letters in range(97, 122):
-      if alpha_letters == ord(e) or alpha_letters == ord(q):
-         continue
-      print("{:c}".format(alpha_letters), end="")
+for a in "abcdefghijklmnopqrstuvwxyz":
+    print('{}'.format(a), end="")

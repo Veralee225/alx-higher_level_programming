fa5fd9902c4b352ccf311a7ebc84a08e5a524510
Veralee225 <Veralee225@gmail.com>
2022-06-22 01:33:18 +0100

    I fixed an error

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
-Square = __import__('0-square').Square
+"""Defines a class Square"""
 
-my_square = Square()
-print(type(my_square))
-print(my_square.__dict__)
+
+class Square:
+    """An empty Square class"""
+    pass

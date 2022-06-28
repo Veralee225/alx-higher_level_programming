3d6bddcdfea69e06d3ee2714a4b4edd000d4ad65
Veralee225 <Veralee225@gmail.com>
2022-06-28 01:40:17 +0100

    Made changes to the function

--- 0x08-python-more_classes/0-rectangle.py
+++ 0x08-python-more_classes/0-rectangle.py
@@ -1,9 +1,12 @@
 #!/usr/bin/python3
+
 """
-This module defines the a Rectangle Object.
+This is the '0-rectangle' module
+Created on Mon 27 2022
+@author: Vera Adiele
 """
 
 
-class Rectangle:
-    """Rectangle class skeleton"""
+class Rectangle():
+    """Empty Class Rectangle that defines a has no method"""
     pass

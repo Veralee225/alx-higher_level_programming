64fcc4d3b2fad66a35fc88b31d188a030786286d
Veralee225 <Veralee225@gmail.com>
2022-10-25 03:28:51 +0100

    feat: tasks 0 & 1

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,5 +1,5 @@
 #!/usr/bin/node
-// A script that reads and prints the content of a file
+// This script that reads and prints the content of a file
 
 const file = process.argv[2];
 const fs = require('fs');
